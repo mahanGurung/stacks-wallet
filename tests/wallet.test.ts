83708f73372af5802d91ac8ca5bf45afe3ae651e
Mahan Gurung <83807944+mahanGurung@users.noreply.github.com>
2025-12-15 17:52:14 +0545

    simple time-lock wallet

--- tests/wallet.test.ts
+++ tests/wallet.test.ts
@@ -1,21 +1,53 @@
 
 import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
 
 const accounts = simnet.getAccounts();
+const deployer = simnet.deployer;
 const address1 = accounts.get("wallet_1")!;
+const address2 = accounts.get("wallet_2")!;
 
 /*
   The test below is an example. To learn more, read the testing documentation here:
   https://docs.hiro.so/stacks/clarinet-js-sdk
 */
+describe("wallet lock lifecycle", () => {
+  it("locks STX", () => {
+    const { result } = simnet.callPublicFn(
+      "wallet",
+      "lock",
+      [
+        Cl.principal(address2),
+        Cl.uint(100),
+        Cl.uint(1000000),
+      ],
+      deployer
+    );
 
-describe("example tests", () => {
-  it("ensures simnet is well initialised", () => {
-    expect(simnet.blockHeight).toBeDefined();
+    expect(result).toBeOk(Cl.bool(true));
   });
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+  it("bestows to new beneficiary", () => {
+    simnet.callPublicFn(
+      "wallet",
+      "lock",
+      [
+        Cl.principal(address2),
+        Cl.uint(100),
+        Cl.uint(1000000),
+      ],
+      deployer
+    );
+
+    const { result } = simnet.callPublicFn(
+      "wallet",
+      "bestow",
+      [Cl.principal(address1)],
+      address2
+    );
+
+    expect(result).toBeOk(Cl.bool(true));
+  });
+
 });
+
